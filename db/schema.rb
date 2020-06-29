e2606122a1f685bc32b31cfbaab132be4560dbf6
webdevjscoder <cobianjuan19@gmail.com>
2020-06-29 09:14:32 -0700

    updated database schema for customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_011021) do
+ActiveRecord::Schema.define(version: 2020_06_29_160911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2020_06_27_011021) do
     t.string "first_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
