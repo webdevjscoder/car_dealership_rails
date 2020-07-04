0c411e841018d57a7d703025c2e9b2c4dce278f4
webdevjscoder <cobianjuan19@gmail.com>
2020-07-03 17:50:10 -0700

    omniauth sign in for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,28 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:user][:email])
+        byebug
+        user = User.find_or_create_by(uid: auth['uid']) do |u|
+            u.name = auth['info']['name']
+            u.email = auth['info']['email']
+        end
+       
         session[:user_id] = user.id
-        redirect_to welcome_path(user), notice: 'You were successfully signed in!'
+       
+        render root_path
+        # user = User.find_by(email: params[:user][:email])
+        # session[:user_id] = user.id
+        # redirect_to welcome_path(user), notice: 'You were successfully signed in!'
     end
 
     def destroy
         session.delete :user_id
         redirect_to root_path, notice: 'You were successfully logged out!'
+    end
+
+    private
+       
+    def auth
+      request.env['omniauth.auth']
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/auth/github/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
