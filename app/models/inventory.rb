419c1f43f1d43f0b99dd9ac07ca6c4175838cf06
webdevjscoder <cobianjuan19@gmail.com>
2020-07-05 17:20:12 -0700

    add links to cartergory options in the homepage

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,9 +1,33 @@
 class Inventory < ApplicationRecord 
     belongs_to :vehicle
 
-    def create_inventory
-        Vehicle.all.each do |vehicle|
-            Inventory.create(vehicle_id: vehicle.id, )
+    def self.economical_vehicles
+        economical_vehicles = []
+        all.each do |vehicle|
+            if vehicle.price.to_i.between?(1000, 25000)
+                economical_vehicles << vehicle
+            end
         end
+        economical_vehicles
     end
+
+    def self.luxury_vehicles
+        luxury_vehicles = []
+        all.each do |vehicle|
+            if vehicle.price.to_i.between?(25000, 65000)
+                luxury_vehicles << vehicle
+            end
+        end
+        luxury_vehicles
+    end
+    
+    def self.exotic_vehicles
+        exotic_vehicles = []
+        all.each do |vehicle|
+            if vehicle.price.to_i.between?(65000, 150000)
+                exotic_vehicles << vehicle
+            end
+        end
+        exotic_vehicles
+    end 
 end
