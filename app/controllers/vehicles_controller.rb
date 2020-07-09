40c91519ea9c106f6170b0131567f9de48adec3e
webdevjscoder <cobianjuan19@gmail.com>
2020-07-08 17:41:39 -0700

    display favorite cars and non purchased cars

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -19,4 +19,17 @@ class VehiclesController < ApplicationController
     def exotic
         @exotic = Inventory.exotic_vehicles.paginate(page: params[:page], per_page: 5)
     end
+
+    def purchase
+        @purchased_vehicle = UserVehicle.find_or_initialize_by(vehicle_id: params[:vehicle][:id], user_id: current_user.id)
+        if @purchased_vehicle.id.present?
+            @purchased_vehicle.vehicle.update(is_purchased: true)
+            @purchased_vehicle.save
+            redirect_to root_path(current_user), alert: "Congratulations! You are now the proud owner of the #{@purchased_vehicle.vehicle.year} " "#{@purchased_vehicle.vehicle.car_make.name} " "#{@purchased_vehicle.vehicle.car_model.name}."
+        else
+            @purchased_vehicle.vehicle.update(is_purchased: true)
+            @purchased_vehicle.save
+            redirect_to root_path(current_user), alert: "Congratulations! You are now the proud owner of the #{@purchased_vehicle.vehicle.year} " "#{@purchased_vehicle.vehicle.car_make.name} " "#{@purchased_vehicle.vehicle.car_model.name}."
+        end
+    end
 end
