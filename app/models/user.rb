882a627a5f87dc93ff2dbebf037f95a8250d4a81
webdevjscoder <cobianjuan19@gmail.com>
2020-07-09 12:05:57 -0700

    admin sign in and sign up working properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
+    attr_accessor :skip_email_validation
+
     has_secure_password
     has_many :user_vehicles
     has_many :vehicles, through: :user_vehicles
-    validate :email_required?
-
-    def email_required?
-        if provider.blank?
-            validates :email, presence:{ message: 'is required' }
-            validates :email, uniqueness: { message: 'already exists!' }
-        end
-    end
-    
+    validates :email, presence:{ message: 'is required.' }, unless: -> { skip_email_validation }
+    validates :email, uniqueness: { message: 'already exists!' }
 end
