e1eba6a3d9df3b401cc8b237019c835e82b28d70
webdevjscoder <cobianjuan19@gmail.com>
2020-07-12 13:26:33 -0700

    update vehicle and delete vehicle actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
-    helper_method :logged_in?, :current_user, :authorized_user
+    helper_method :logged_in?, :current_user, :authorized_user, :admin_user
 
     protected
 
@@ -14,6 +14,14 @@ class ApplicationController < ActionController::Base
 
     def authorized_user
         if !logged_in?
+            redirect_to signin_path
+        end
+    end
+
+    def admin_user
+        if !current_user.admin == true
+            redirect_to welcome_path(current_user)
+        elsif !logged_in?
             redirect_to signin_path
         end
     end
