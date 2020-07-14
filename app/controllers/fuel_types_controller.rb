f7534dcdbe3b89899c49d22b31f61818bffeb892
webdevjscoder <cobianjuan19@gmail.com>
2020-07-14 12:42:18 -0700

    create new specs for vehicles

--- app/controllers/fuel_types_controller.rb
+++ app/controllers/fuel_types_controller.rb
@@ -1,5 +1,10 @@
 class FuelTypesController < ApplicationController
 
+    def index
+        @fuel_types = FuelType.all
+        render :layout => 'forms'
+    end
+
     def new
         @fuel_types = FuelType.all
         @fuel_type = FuelType.new
@@ -7,7 +12,31 @@ class FuelTypesController < ApplicationController
     end
 
     def create
-        @fuel_type = FuelType.new(fuel_type_params(:name, :city_mpg, :hwy_mpg))
+        fuel_type = FuelType.new(fuel_type_params(:name, :city_mpg, :hwy_mpg))
+        if fuel_type.save
+            flash[:notice] = 'Fuel type was successfully created!'
+            redirect_to new_vehicle_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @fuel_type = FuelType.find_by_id(params[:id])
+        render :layout => 'forms'
+    end
+
+    def update
+        fuel_type = FuelType.find_by_id(params[:id])
+        fuel_type.update(fuel_type_params(:name, :city_mpg, :hwy_mpg))
+        fuel_type.save
+        redirect_to new_vehicle_path
+    end
+
+    def destroy
+        FuelType.find_by_id(params[:id]).destroy
+        flash[:notice] = "Successfully deleted!"
+        redirect_to new_vehicle_path
     end
 
     private
--- app/controllers/transmissions_controller.rb
+++ app/controllers/transmissions_controller.rb
@@ -1,12 +1,41 @@
 class TransmissionsController < ApplicationController
 
+    def index
+        @transmissions = Transmission.all
+        render :layout => 'forms'
+    end
+
     def new
         @transmission = Transmission.new
         render :layout => 'forms'
     end
 
     def create
-        @transmission = Transmission.new(transmission_params(:name))
+        transmission = Transmission.new(transmission_params(:name))
+        if transmission.save
+            flash[:notice] = 'Transmission was successfully created!'
+            redirect_to new_vehicle_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @transmission = Transmission.find_by_id(params[:id])
+        render :layout => 'forms'
+    end
+
+    def update
+        transmission = Transmission.find_by_id(params[:id])
+        transmission.update(transmission_params(:name))
+        transmission.save
+        redirect_to new_vehicle_path
+    end
+
+    def destroy
+        Transmission.find_by_id(params[:id]).destroy
+        flash[:notice] = "Successfully deleted!"
+        redirect_to new_vehicle_path
     end
 
     private
