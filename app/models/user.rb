1a918b988d6cff7c0f598b99ac894d464e831d8e
webdevjscoder <cobianjuan19@gmail.com>
2020-07-14 20:06:55 -0700

    delete user and vehicle connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     attr_accessor :skip_email_validation
 
     has_secure_password
-    has_many :user_vehicles
+    has_many :user_vehicles, :dependent => :destroy
     has_many :vehicles, through: :user_vehicles
     validates :email, presence:{ message: 'is required.' }, unless: -> { skip_email_validation }
     validates :email, uniqueness: { message: 'already exists!' }
